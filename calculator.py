f471c01575ea6476f91d52f55703123868bfb344
Caroline Brasil <caroline.brasil@telefonica.com>
2021-04-16 15:38:19 -0300

    ✏️ fix: typos and whitespaces

--- calculator.py
+++ calculator.py
@@ -15,6 +15,7 @@ def subtract(first_term, second_term):
 def multiply(first_term, second_term):
     return first_term * second_term
 
+
 def divide(first_term, second_term):
     return first_term / second_term
 # This is a new line that ends the file.
