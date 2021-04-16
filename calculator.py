7c7b68437d1063a1b348ae86e30a3f5ba29dd15f
Caroline Brasil <caroline.brasil@telefonica.com>
2021-04-16 11:01:45 -0300

    ✨ Add functions for addition and subtraction

---
+++ calculator.py
@@ -0,0 +1,10 @@
+"""
+calculator.py
+Calculator library containing basic math operations.
+"""
+
+def add(first_term, second_term):
+    return first_term + second_term
+
+def subtract(first_term, second_term):
+    return first_term - second_term
