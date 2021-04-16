ec102c3e4731c0786e64eeaf45ac8bb553b5d409
Caroline Brasil <caroline.brasil@telefonica.com>
2021-04-16 15:08:58 -0300

    ✅ test: add test for multiplication

--- test_calculator.py
+++ test_calculator.py
@@ -12,3 +12,6 @@ class TestCalculator:
     def test_subtraction(self):
         assert 2 == calculator.subtract(4, 2)
 # This is a new line that ends the file.
+
+    def test_multiplaction(self):
+        assert 100 == calculator.multiply(10, 10)
