6e1a96002ec3d03c8bed4edb926483eb5063d945
Caroline Brasil <caroline.brasil@telefonica.com>
2021-04-16 15:00:57 -0300

    ✅ Fix typos and W292

--- calculator.py
+++ calculator.py
@@ -10,3 +10,4 @@ def add(first_term, second_term):
 
 def subtract(first_term, second_term):
     return first_term - second_term
+# This is a new line that ends the file.
--- test_calculator.py
+++ test_calculator.py
@@ -8,6 +8,7 @@ import calculator
 class TestCalculator:
     def test_add(self):
         assert 4 == calculator.add(2, 2)
-    
+
     def test_subtraction(self):
         assert 2 == calculator.subtract(4, 2)
+# This is a new line that ends the file.
