58c2b79e92f81d5b414e5a527dcf2de960a69f48
Caroline Brasil <caroline.brasil@telefonica.com>
2021-04-16 15:23:10 -0300

    ✏️ fix: function name

--- test_calculator.py
+++ test_calculator.py
@@ -12,6 +12,6 @@ class TestCalculator:
     def test_subtraction(self):
         assert 2 == calculator.subtract(4, 2)
 
-    def test_multiplaction(self):
+    def test_multiplication(self):
         assert 100 == calculator.multiply(10, 10)
 # This is a new line that ends the file.
